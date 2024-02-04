645e15701f110282994c6df37c2b37bdaaa4598c
shubhamrustagi <rustagishubham2000@gmail.com>
2024-02-04 08:54:30 +0530

    added chart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
  
 export default async function Page() {
   return (
